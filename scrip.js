37a6e5f25231f7e5a00aded28d1a26cc4a571526
Johongir10 <shavkatovj73@gmail.com>
2024-06-26 10:32:48 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,13 @@
+let ism = prompt("Ismingizni kiriting");
+let familiya = prompt("Familiyangizni kiriting");
+let Ochistva  =prompt("Ochistvani kiriting") ;      
+let yosh   =prompt("Yoshingizni kiriting");
+let Turish  =prompt("Turish joyingizni kiriting");
+
+
+
+console.log("Sizning turish manziliz"  +Turish);
+console.log("Sizning yoshingiz" + yosh)
+console.log("Sizning Ochistvangiz"+   Ochistva);
+console.log(" Sizning Familiyangiz"+   familiya);
+console.log("Sizning ismingiz:"+    ism);
